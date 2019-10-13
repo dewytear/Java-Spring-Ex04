1b723145c125e364d727801db20cf0dfe101caa4
dewytear <youngjinkwak@gmail.com>
2019-10-13 18:14:08 +0900

    20191013

--- src/main/java/com/spring/annotationEx/AbstractController.java
+++ src/main/java/com/spring/annotationEx/AbstractController.java
@@ -28,12 +28,15 @@ import org.springframework.web.servlet.ModelAndView;
  **/
 //@Component
 @Controller
-public class AbstractController extends org.springframework.web.servlet.mvc.AbstractController{
-	
+public class AbstractController extends org.springframework.web.servlet.mvc.AbstractController{	
 	// AbstractController 는 org.springframework.web.servlet.mvc의 최상위 컨트롤러로써..
-	// 컨트롤러를 추상화한 것으로 이런예제에서 많이 사용된다.
-	@Resource(name="service01")
-	private AbstractSvc service;
+	// 컨트롤러를 추상화한 것으로 이런 @Controller Annotation 설명하는 예제에서 많이 사용된다.
+	
+	// @Resource Annotation을 이용하여 "service01"이라는 이름의 Bean을 이용한다.
+	// 여기서는 AbstractSvcImpl()에서 Baan 생성된다.
+	@Resource(name="service02")
+	
+	private AbstractSvc service;	//인터페이스
 	
 	// AbstractController의 handleRequestInternal 를 오버라이드 하여 예제로 사용
 	@Override
--- src/main/webapp/WEB-INF/views/componentView.jsp
+++ src/main/webapp/WEB-INF/views/componentView.jsp
@@ -4,11 +4,11 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>@Resource 사용 예제</title>
 </head>
 <body>
 
-	<h2>${"msg"}</h2>
+	<h2>${msg}</h2>
 
 </body>
 </html>
